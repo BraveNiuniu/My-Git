1b047fee1ad5fe1c67cd8e02dd4239f2c05f34a1
${BraveNiuniu} <${1831218952@qq.com}>
2021-06-27 20:10:25 +0800

    第三次测试提交

--- demo-moudle-1/src/com/duyi/Demo1.java
+++ demo-moudle-1/src/com/duyi/Demo1.java
@@ -2,6 +2,8 @@ package com.duyi;
 
 public class Demo1 {
     public static void main(String[] args) {
+        System.out.println("Hello Git");
+
         System.out.println("Hello test");
     }
 }
