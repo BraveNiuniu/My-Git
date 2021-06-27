50b4d68bd04b7249c6f37f02b885ec16a376f690
${BraveNiuniu} <${1831218952@qq.com}>
2021-06-27 23:01:38 +0800

    提交hello master

--- demo-moudle-1/src/com/duyi/Demo.java
+++ demo-moudle-1/src/com/duyi/Demo.java
@@ -3,5 +3,7 @@ package com.duyi;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+
+        System.out.println("Hello master");
     }
 }
